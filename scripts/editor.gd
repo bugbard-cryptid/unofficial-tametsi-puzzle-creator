9f24e2b54372537ffd49eec4dc156a927debf17a
wn <61643906+bugbard-cryptid@users.noreply.github.com>
2024-06-08 22:54:12 -0700

    load tile colors!

--- scripts/editor.gd
+++ scripts/editor.gd
@@ -3,6 +3,18 @@ extends Node
 var xml = XMLParser.new()
 var tileScene = preload("res://scenes/tile.tscn")
 
+var validColors = {
+	"RED": Color("#d82825"), 
+	"ORANGE": Color("#ea8b25"), 
+	"YELLOW": Color("#dad200"), 
+	"GREEN": Color("#70d637"), 
+	"BLUE": Color("#52b3ff"), 
+	"INDIGO": Color("#4545ea"), 
+	"VIOLET": Color("#9000ea"), 
+	"PINK": Color("#ea33ea"),
+	"GREY": Color("#7e7e7e")
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var uiNode = get_node("CanvasLayer/UI")
@@ -32,7 +44,7 @@ func load_level(level):
 			print("TITLE: " + xml.get_node_data())
 			break
 	
-	# generate tiles + print tile count
+	# load tiles + print tile count
 	var tileCount = 0
 	xml.seek(0)
 	while xml.read() != ERR_FILE_EOF:
@@ -70,3 +82,40 @@ func load_level(level):
 			$Puzzle.add_child(tileInstance)
 	print(str($Puzzle.get_child_count()) + " tiles")
 	
+	# load hints (tile colors, color tiles based on the IDs listed in <HINTS_LIST>)
+	xml.seek(0)
+	while xml.read() != ERR_FILE_EOF:
+		if xml.get_node_type() == xml.NODE_ELEMENT and xml.get_node_name() == "HINT_LIST":
+			while not (xml.get_node_type() == xml.NODE_ELEMENT_END and xml.get_node_name() == "HINT_LIST"): # until we reach </HINT_LIST>
+				xml.read()
+				if xml.get_node_type() == xml.NODE_ELEMENT and xml.get_node_name() == "HINT":
+					var coloredTiles = []
+					var tiledColors # naming variables like this is bad practice i think but its funny hee he :3
+					var isDark = false
+					while not (xml.get_node_type() == xml.NODE_ELEMENT_END and xml.get_node_name() == "HINT"): # until we reach the end of this <HINT>
+						xml.read()
+						if xml.get_node_type() == xml.NODE_ELEMENT and xml.get_node_name() == "IDS":
+							xml.read()
+							coloredTiles = xml.get_node_data().split(",")
+						if xml.get_node_type() == xml.NODE_ELEMENT and xml.get_node_name() == "COLOR":
+							xml.read()
+							tiledColors = xml.get_node_data()
+						if xml.get_node_type() == xml.NODE_ELEMENT and xml.get_node_name() == "IS_DARK":
+							xml.read()
+							isDark = xml.get_node_data()
+							if isDark == "true":
+								isDark = true
+							else:
+								isDark = false
+					for tile in $Puzzle.get_children():
+						if tile.ID in coloredTiles:
+							tile.COLOR = tiledColors
+							tile.get_node("Polygon").color = validColors.get(tiledColors)
+							if isDark:
+								tile.get_node("Polygon").color *= Color("#eee")
+	# load column hints
+	xml.seek(0)
+	if xml.get_node_type() == xml.NODE_ELEMENT and xml.get_node_name() == "COLUMN_HINT_LIST":
+		pass
+	
+	print("done loading!!!")
