c191ba5ed1aa789a11a248b555279fe188b7b793
wren <wilrnilr@gmail.com>
2025-05-30 00:15:10 -0700

    restore tile.gd

---
+++ scripts/tile.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var ID = 0
+var EDGES = []
+var COLOR
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$IDlabel.text = str(ID)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.Add commentMore actions
+func _process(delta):
+	pass
